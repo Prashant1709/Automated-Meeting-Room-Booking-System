51179375a3541604bd9356b31b39d6892901768a
Bilal <bsajid173@gmail.com>
2024-08-24 20:04:12 +0530

    fixes

--- application/FrontEnd/AdminPage/ManageUsers/manage-users.js
+++ application/FrontEnd/AdminPage/ManageUsers/manage-users.js
@@ -146,3 +146,16 @@ const logoutHandler = () => {
 const goToHome = () => {
   window.location.href = "/application/FrontEnd/Homepage/index.html";
 };
+
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
